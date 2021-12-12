7c806968611aee993fca67282b2c4302f429c677
Simplestart2021 <junaid.nur@outlook.com>
2021-12-11 22:19:56 -0500

    created a welcome controller

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -1,18 +1,17 @@
-# class ArticlesController < ApplicationController
-#   http_basic_authenticate_with name: "jn", password: "junaid",
-#   except: [:index, :show]
-#
-# def new
-#   @article = Article.new
-# end
-#
-# def create
-#   @article = Article.new(article_params)
-#
-#   if @article.save
-#     redirect_to @article
-#   else
-#     render 'new'
-#   end
-# end
-#
+class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "jn", password: "junaid",
+  except: [:index, :show]
+
+def new
+  @article = Article.new
+end
+
+def create
+  @article = Article.new(article_params)
+
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
+end
