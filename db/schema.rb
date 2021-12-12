daeaa203c2cb5b214d05dbbf9b1c5f63c62a9581
Simplestart2021 <junaid.nur@outlook.com>
2021-12-12 06:11:02 -0500

    adding user, creating 1 to many relationship with articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_12_103206) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
